6a7e6caacd0c9b919455e500eb519a873e862f92
John Rapp <johnfarnes@hotmail.com>
2015-10-02 22:38:23 +0200

    Started curve fever prototype

---
+++ Curvefever/public/controller/controller.js
@@ -0,0 +1,72 @@
+'use strict';
+angular.module('app', ['ngMaterial', 'lib'])
+.config(['socketProvider', function(socketProvider) {
+	socketProvider.namespace = 'controller';
+}])
+.controller('controller', ['$scope', 'socket', function($scope, socket) {
+	$scope.refresh = function() {
+		socket.emit('refreshDisplay');
+	};
+	$scope.rotate = function() {
+		socket.emit('rotateDisplay');
+	};
+}])
+.directive('touchArea', ['socket', function(socket) {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attr) {
+			// element[0].addEventListener('touchstart', function(e) {
+			// 	var touch = e.touches[0];
+			// 	var position = {x: touch.clientX / element[0].offsetWidth, y: touch.clientY / element[0].offsetHeight};
+
+			// 	socket.emit('touchstart', position);
+			// }, false);
+
+			// element[0].addEventListener('touchend', function(e) {
+			// 	socket.emit('touchend');
+			// }, false);
+			
+			// element[0].addEventListener('touchcancel', function(e) {
+			// 	socket.emit('touchend');
+			// }, false);
+			
+			// element[0].addEventListener('touchmove', function(e) {
+			// 	var touch = e.touches[0];
+			// 	var position = {x: touch.clientX / element[0].offsetWidth, y: touch.clientY / element[0].offsetHeight};
+
+			// 	socket.emit('touchmove', position);
+			// }, false);
+
+			var hammertime = new Hammer(element[0], {});
+			hammertime.get('pinch').set({ enable: true });
+			hammertime.get('rotate').set({ enable: true });
+
+			hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+
+			var position = null;
+			hammertime.on('panstart', function(e) {
+				position = {x: e.deltaX, y: e.deltaY};
+			});
+			hammertime.on('panend', function(e) {
+				position = null;
+			});
+
+			hammertime.on('panmove', function(e) {
+				if(position) {
+					var delta = {x: (e.deltaX - position.x), y: (e.deltaY - position.y)};
+					console.log(e.deltaX, e.deltaY);
+					socket.emit('pan', delta);
+					position = {x: e.deltaX, y: e.deltaY};
+				}
+			});
+
+			hammertime.on('rotate', function(e) {
+				socket.emit('rotateCube', e.rotation);
+			});
+
+			hammertime.on('pinch', function(e) {
+				socket.emit('scale', e.scale);
+			});
+		}
+	}
+}]);
--- Curvefever/public/display/display.js
+++ Curvefever/public/display/display.js
@@ -339,4 +339,268 @@ angular.module('app', ['ngMaterial', 'lib'])
 			});
 		}
 	};
+}])
+.directive('curveFever', ['touch', 'size', 'socket', function(touch, size, socket) {
+	return {
+		restrict: 'E',
+		replace: true,
+		template: '<div></div>',
+		link: function(scope, element, attr) {
+			var w = size, h = size / 2;
+
+			var scene = new THREE.Scene();
+			var camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+
+			var renderer = new THREE.WebGLRenderer();
+			renderer.setSize(w, h);
+			element.append( renderer.domElement );
+
+			// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			// var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+			// material.opacity = 0.01;
+			// var cube = new THREE.Mesh( geometry, material );
+			// scene.add( cube );
+
+			var light = new THREE.PointLight( 0xffffff, 1, 100 );
+			light.position.set( 0, 0, 5 );
+			scene.add( light );
+
+			var a = 10, b = 10, c = 10;
+
+			var world = [];
+			for(var i = 0; i < a; i++) {
+				world.push([]);
+				for(var j = 0; j < b; j++) {
+					world[i].push([0, 0, 0, 0]);
+				}
+			}
+
+			var worldObjects = [];
+			for(var i = 0; i < a; i++) {
+				worldObjects.push([]);
+				for(var j = 0; j < b; j++) {
+					worldObjects[i].push([]);
+				}
+			}
+
+			generateScene(a, b, c);
+
+
+			var player = {
+				pos: new THREE.Vector3(0, 0, c - 1),
+				dir: new THREE.Vector3(1, 0, 0)
+			};
+
+			function generateScene(a, b, c) {
+				var padding = 0.1;
+				var scale = .5;
+				// var offset = new THREE.Vector3(-a / 2, -b / 2, -c / 2);
+				var offset = new THREE.Vector3(-a / 4, -b / 4, -c / 2);
+
+				for(var x = 0; x < a; x++) {
+					for(var y = 0; y < b; y++) {
+						for(var z = 0; z < c; z++) {
+							var geometry = new THREE.BoxGeometry( scale, scale, scale );
+							var material = new THREE.MeshLambertMaterial( { color: 0xffffff, transparent: true } );
+							material.opacity = 0.4;
+							var cube = new THREE.Mesh( geometry, material );
+							cube.position.set(x * scale + padding * x + offset.x, y * scale + padding * y + offset.y, z * scale + padding * z + offset.z);
+							scene.add( cube );
+							worldObjects[x][y][z] = cube;
+						}
+					}
+				}
+
+			}
+
+			// var loader = new THREE.OBJMTLLoader();
+
+			// var object;
+
+			// load an obj / mtl resource pair
+			// loader.load(
+			// 	// OBJ resource URL
+			// 	'obj/THACUBE.obj',
+			// 	// MTL resource URL
+			// 	'obj/THACUBE.mtl',
+			// 	// Function when both resources are loaded
+			// 	function ( _object ) {
+			// 		object = _object;
+			// 		// object.scale.set(3, 3, 3);
+			// 		object.position.set(0, -3, -5);
+			// 		scene.add( object );
+
+			// 		object.children.forEach(function(child) {
+			// 			child.geometry.computeVertexNormals();
+			// 			child.geometry.mergeVertices();
+			// 		});
+
+			// 		var light = new THREE.PointLight( 0xffffff, 1, 100 );
+			// 		light.position.set( 0, 0, 5 );
+			// 		scene.add( light );
+			// 		// var cube = new THREE.Mesh( geometry, material );
+			// 		// scene.add( cube );
+			// 	},
+			// 	// Function called when downloads progress
+			// 	function ( xhr ) {
+			// 		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+			// 	},
+			// 	// Function called when downloads error
+			// 	function ( xhr ) {
+			// 		console.log( 'An error happened' );
+			// 	}
+			// );
+
+			// instantiate a loader
+			// var loader = new THREE.JSONLoader();
+
+			// // load a resource
+			// loader.load(
+			// 	// resource URL
+			// 	'benanimation.js',
+			// 	// Function when resource is loaded
+			// 	function ( geometry, materials ) {
+			// 		var material = new THREE.MeshFaceMaterial( materials );
+			// 		var object = new THREE.Mesh( geometry, material );
+			// 		scene.add( object );
+			// 	}
+			// );
+			// var loader = new THREE.SceneLoader();
+
+			// loader.load( "benanimation.js", function(result) {
+			// 	console.log(result)
+			// } );
+
+			 // var loader = new THREE.ObjectLoader();
+    //         // loader.load( 'scene.json', function ( object ) {
+    //         loader.load( 'benanimation.json', function ( object ) {
+
+    //                     scene = object;
+
+    //                     var light = new THREE.PointLight( 0xffffff, 1, 100 );
+				// 	light.position.set( 0, 0, 5 );
+				// 	scene.add( light );
+
+    //         } );
+
+			// function morphColorsToFaceColors( geometry ) {
+
+			// 	if ( geometry.morphColors && geometry.morphColors.length ) {
+
+			// 		var colorMap = geometry.morphColors[ 0 ];
+
+			// 		for ( var i = 0; i < colorMap.colors.length; i ++ ) {
+
+			// 			geometry.faces[ i ].color = colorMap.colors[ i ];
+			// 			geometry.faces[ i ].color.offsetHSL( 0, 0.3, 0 );
+
+			// 		}
+
+			// 	}
+
+			// }
+
+			camera.position.z = 5;
+
+			function update(time) {
+				// if(object) {
+					// object.position.set(0, 0, 0)
+					// object.rotation.x += 0.1;
+					// object.rotation.y += 0.1;
+					// object.position.set(0, -10, 0)
+				// }
+
+				// function randomInt(x) {return Math.floor(Math.random() * x);}
+				// world[randomInt(a)][randomInt(b)][randomInt(c)].material.color = 0xff0000;
+			}
+
+			keyboard.listen();
+			setInterval(function update() {
+				if(keyboard.isDown('left')) {
+					player.dir.applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI / 2);
+				}
+				if(keyboard.isDown('right')) {
+					player.dir = new THREE.Vector3(1, 0, 0);
+				
+				}
+				if(keyboard.isDown('up')) {
+				
+				}
+				if(keyboard.isDown('down')) {
+				
+				}
+
+				// var object = worldObjects[player.pos.x][player.pos.y][player.pos.z];
+				// object.material.color.setHex(0xff0000);
+				// player.pos = player.pos.add(player.dir);
+				window.dir = player.dir;
+			}, 300);
+
+			requestAnimationFrame(function render(time) {
+				update(time);
+				
+				renderer.render( scene, camera );
+
+				requestAnimationFrame(render);
+			});
+		}
+	};
 }]);
+
+(function() {'use strict';
+
+	var keys = {}, keyMap = {
+		37: 'left',
+		39: 'right',
+		38: 'up',
+		40: 'down',
+
+		87: 'w',
+		65: 'a',
+		83: 's',
+		68: 'd',
+
+		13: 'enter',
+		32: 'space'
+	};
+
+	window.keys = keys;
+
+	function listen() {
+		window.addEventListener('keydown', onkeydown);
+		window.addEventListener('keyup', onkeyup);
+	}
+
+	function cancel() {
+		window.removeEventListener('keydown', onkeydown);
+		window.removeEventListener('keyup', onkeyup);
+	}
+
+	function onkeydown(e) {
+		if(keyMap.hasOwnProperty(e.which)) {
+			keys[keyMap[e.which]] = 1;
+			e.preventDefault();
+		}
+	}
+
+	function onkeyup(e) {
+		if(keyMap.hasOwnProperty(e.which)) {
+			keys[keyMap[e.which]] = 0;
+			e.preventDefault();
+		}
+	}
+
+	function keydown(key) {
+		return keys[key];
+	}
+
+	window.keyboard = {
+		isDown: keydown,
+		listen: listen,
+		cancel: cancel,
+		mapKey: function(which) {
+			return keyMap[which];
+		}
+	};
+
+})();
